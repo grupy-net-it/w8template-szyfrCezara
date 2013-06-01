676e50c806d1d1bc9b5f600a96fa7dbd7b8f210c
Kamil Cie�lak <keraxel@gmail.com>
2013-06-01 20:10:28 +0200

    Init commit

---
+++ SzyfrCezara2/Szyfrowanie.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzyfrCezara2
+{
+    class Szyfrowanie
+    {
+
+        private string x;
+
+        public string szyfr(string wiadomosc, int n)
+        {
+            x = "";
+
+            for (int i = 0; i < wiadomosc.Length; i++)
+            {
+                if (Convert.ToInt32(wiadomosc[i]) > 64 && Convert.ToInt32(wiadomosc[i])<91 && Convert.ToInt32(wiadomosc[i])+n>90)
+                {
+                    x = x+Convert.ToChar((Convert.ToInt32(wiadomosc[i]) + n) - 26);
+                }
+                else if (Convert.ToInt32(wiadomosc[i]) > 96 && Convert.ToInt32(wiadomosc[i]) < 123 && Convert.ToInt32(wiadomosc[i]) + n > 122)
+                {
+                    x = x + Convert.ToChar((Convert.ToInt32(wiadomosc[i]) + n) - 26);
+                }
+                else if (Convert.ToInt32(wiadomosc[i]) > 47 && Convert.ToInt32(wiadomosc[i]) < 58 && Convert.ToInt32(wiadomosc[i]) + n > 57)
+                {
+                    x = x + Convert.ToChar((Convert.ToInt32(wiadomosc[i]) + n) - 26);
+                }
+                else
+                x = x+Convert.ToChar((Convert.ToInt32(wiadomosc[i])+n));
+            }
+
+                return x; 
+        }
+        public string deszyfr(string wiadomosc, int n)
+        {
+            x = "";
+
+            for (int i = 0; i < wiadomosc.Length; i++)
+            {
+                if (Convert.ToInt32(wiadomosc[i]) > 64 && Convert.ToInt32(wiadomosc[i]) < 91 && Convert.ToInt32(wiadomosc[i]) - n < 65)
+                {
+                    x = x + Convert.ToChar((Convert.ToInt32(wiadomosc[i]) - n) + 26);
+                }
+                else if (Convert.ToInt32(wiadomosc[i]) > 96 && Convert.ToInt32(wiadomosc[i]) < 123 && Convert.ToInt32(wiadomosc[i]) - n < 97)
+                {
+                    x = x + Convert.ToChar((Convert.ToInt32(wiadomosc[i]) - n) + 26);
+                }
+                else if (Convert.ToInt32(wiadomosc[i]) > 47 && Convert.ToInt32(wiadomosc[i]) < 58 && Convert.ToInt32(wiadomosc[i]) - n < 57)
+                {
+                    x = x + Convert.ToChar((Convert.ToInt32(wiadomosc[i]) - n) + 26);
+                }
+                else
+                    x = x + Convert.ToChar((Convert.ToInt32(wiadomosc[i]) - n));
+            }
+
+            return x;
+        }
+
+    }
+}
